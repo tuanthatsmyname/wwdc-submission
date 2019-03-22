86bdae884be358778888316a66afb1e3e922d716
Tuan Tu Do <tuanthatsmyname@gmail.com>
2019-03-23 00:55:02 +0100

    animating cursor done, saving and loading terminal history done

--- PlaygroundBook/Sources/LiveViewController_1_2.swift
+++ PlaygroundBook/Sources/LiveViewController_1_2.swift
@@ -44,38 +44,57 @@ public class LiveViewController_1_2: LiveViewController {
 
     }
     
-    // TODO: show the whole history
+    // TODO: load history
     private func showPrompt() {
-        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
-            if self.counter % 2 == 0 {
-                self.terminalTextView.attributedText = self.terminalSettings.prompt
-            } else {
-                let attributedString = NSMutableAttributedString(attributedString: self.terminalSettings.prompt)
-                attributedString.append(NSMutableAttributedString(string: "│", attributes: self.terminalSettings.boldAttributes))
-                self.terminalTextView.attributedText = attributedString
+        
+        if let terminalHistory = DataManager.loadTerminalHistory() {
+            terminalTextView.attributedText = terminalHistory
+            animateCursor()
+        } else {
+            timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
+                if self.counter % 2 == 0 {
+                    let attributedString = NSMutableAttributedString(attributedString: self.terminalSettings.prompt)
+                    attributedString.append(NSMutableAttributedString(string: "│", attributes: self.terminalSettings.normalAttributes))
+                    self.terminalTextView.attributedText = attributedString
+                } else {
+                    self.terminalTextView.attributedText = self.terminalSettings.prompt
+                }
+                self.counter += 1
             }
-            self.counter += 1
         }
+    
     }
     
-    // TODO: show history
-    // TODO: delete "|" from the animation
-    // TODO: scroll when the textview is full
-    // TODO: show "|" when entering a new command
     private func executeCommand(command: String) {
         timer.invalidate()
         var index = 0
         
         let _ = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
-            let attributedText = NSMutableAttributedString(attributedString: self.terminalTextView.attributedText)
-            attributedText.append(NSMutableAttributedString(string: String(command.characters[index]), attributes: self.terminalSettings.normalAttributes))
-            self.terminalTextView.attributedText = attributedText
+            
+            if index < command.count {
+                let attributedText = NSMutableAttributedString(attributedString: self.terminalTextView.attributedText)
+                
+                // deleting "|" from the prompt animation
+                if index == 0, attributedText.string.last! == "│" {
+                    attributedText.deleteCharacters(in: NSRange(location: attributedText.length - 1, length: 1))
+                }
+                
+                // deleting "|" from the type animation
+                if index != 0 {
+                    attributedText.deleteCharacters(in: NSRange(location: attributedText.length - 1, length: 1))
+                }
+                attributedText.append(NSMutableAttributedString(string: String(command.characters[index]) + "│", attributes: self.terminalSettings.normalAttributes))
+                self.terminalTextView.attributedText = attributedText
+            }
 
             index += 1
 
-            if index == command.characters.count {
+            if index == command.count + 3 {
                 timer.invalidate()
+                
                 let attributedText = NSMutableAttributedString(attributedString: self.terminalTextView.attributedText)
+                // deleting "|" from the type animation
+                attributedText.deleteCharacters(in: NSRange(location: attributedText.length - 1, length: 1))
                 attributedText.append(NSMutableAttributedString(string: "\n"))
                 if let result = self.terminal.execute(command: command) {
                     attributedText.append(NSMutableAttributedString(string: result, attributes: self.terminalSettings.normalAttributes))
@@ -84,8 +103,34 @@ public class LiveViewController_1_2: LiveViewController {
                 }
                 attributedText.append(self.terminalSettings.prompt)
                 self.terminalTextView.attributedText = attributedText
+                
+                if self.terminalTextView.contentSize.height > self.terminalTextView.frame.height {
+                    let range = NSMakeRange(self.terminalTextView.attributedText.length - 1, 1)
+                    self.terminalTextView.scrollRangeToVisible(range)
+                }
+                
+                DataManager.saveTerminalHistory(from: attributedText)
+                
+                self.animateCursor()
+                
             }
 
+        }
+    }
+    
+    private func animateCursor() {
+        counter = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
+            if self.counter % 2 == 0 {
+                let attributedText = NSMutableAttributedString(attributedString: self.terminalTextView.attributedText)
+                attributedText.append(NSMutableAttributedString(string: "│", attributes: self.terminalSettings.normalAttributes))
+                self.terminalTextView.attributedText = attributedText
+            } else {
+                let attributedText = NSMutableAttributedString(attributedString: self.terminalTextView.attributedText)
+                attributedText.deleteCharacters(in: NSRange(location: attributedText.length - 1, length: 1))
+                self.terminalTextView.attributedText = attributedText
+            }
+            self.counter += 1
         }
     }
     
@@ -101,8 +146,3 @@ public class LiveViewController_1_2: LiveViewController {
         
     }
 }
-
-//                if self.terminalTextView.contentSize.height > self.terminalTextView.frame.height {
-//                    let range = NSMakeRange(self.terminalTextView.attributedText.length - 1, 1)
-//                    self.terminalTextView.scrollRangeToVisible(range)
-//                }
