e96aba11a8d3c59441a298c5c2f493b20a721c08
Tuan Tu Do <tuanthatsmyname@gmail.com>
2019-03-23 16:08:23 +0100

    rest of the pages added and named

---
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage9.playgroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//#-hidden-code
+
+import Foundation
+
+//#-end-hidden-code
+
+let command: String = /*#-editable-code*//*#-end-editable-code*/
+    
+    //#-hidden-code
+    
+    sendValue(.data(try NSKeyedArchiver.archivedData(withRootObject: command, requiringSecureCoding: true)))
+
+//#-end-hidden-code
+
+
